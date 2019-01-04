18b452ed9e22d953929e2246ec7362a7eac33ab1
Lee <liam.eakins@outlook.com>
2019-01-04 03:55:38 -0600

    Complete functions
    Still is buggy when I try to reduce playerWins and computerWins back to 0, tried a few different ways and none worked so I removed that feature.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let playerWins = 0;
+let computerWins = 0;
+
 // Function Returns Computer's Choice
 function computerPlay() {
   var random = Math.floor((Math.random() * 3) + 1);
@@ -27,20 +30,34 @@ function userPlay() {
 }
 
 
-function roundPlay() {
+function playRound() {
   let computerSelection = computerPlay();
   let playerSelection = userPlay();
   if (computerSelection == "rock" && playerSelection == "rock" || computerSelection == "scissors" && playerSelection == "scissors" || computerSelection == "paper" && playerSelection == "paper") {
     return "Tie!";
   }
     else if (computerSelection == "rock" && playerSelection == "scissors" || computerSelection == "paper" && playerSelection == "rock" || computerSelection == "scissors" && playerSelection == "paper") {
+      computerWins++;
       return "Computer Wins!";
     }
     else {
+      playerWins++;
       return "User Wins!";
     }
 }
 
-function gamePlay() {
-  
+function playGame() {
+
+  for (let i = 0; i < 5; i++) {
+    playRound();
+    console.log("Round End! You: " + playerWins + " Computer Wins: " + computerWins);
+  }
+  if (playerWins > computerWins) {
+    return "You win the game, congratulations! You are very skilled!";
+  }
+  else {
+    return "The computer wins the game, you bad.";
+  }
 }
+
+
